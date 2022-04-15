4c2f516d4f5b39baeda3560f372ec3862f428312
Simon Chabrier <simonchabrier@MacBook-Pro-de-Simon.local>
2022-04-15 16:57:46 +0200

    first commit on develop

---
+++ README.md
@@ -0,0 +1,5 @@
+### Routes POST et GET
+
+pour gérer le back office de jsVideoStreamAndCapture
+
+https://js.simschab.fr/cam/index.html
---
+++ src/Entity/Picture.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PictureRepository;
+// use Gedmo\Mapping\Annotation as Gedmo;
+// @Gedmo\Timestampable(on="create")
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=PictureRepository::class)
+ */
+class Picture
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $picture;
+
+    
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPicture(): ?string
+    {   
+       
+        return $this->picture;
+    }
+
+    public function setPicture(string $picture): self
+    {
+
+        $this->picture = $picture;
+  
+        return $this;
+    }
+
+}
