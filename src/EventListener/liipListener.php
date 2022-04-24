8150996c46ed4e3c6550f3135cbe582af215cfaa
Simon Chabrier <simonchabrier@MacBook-Pro-de-Simon.local>
2022-04-25 00:00:15 +0200

    add comments on liipListener.php

--- src/EventListener/liipListener.php
+++ src/EventListener/liipListener.php
@@ -5,11 +5,19 @@ namespace App\EventListener;
 use App\Entity\Picture;
 use Liip\ImagineBundle\Service\FilterService;
 
-//todo ATTENTION CE LISTENER A ETE RENOMME ET CA A CREE UN CACHE OU UEN ENREGISTREMENT QUELQUE PART !
-//todo  IL EST REFERENCE AVEC UN 'l' MINUSCULE A liip  
+// todo ATTENTION CE LISTENER A ETE RENOMME ET CA A CREE UN CACHE OU UEN ENREGISTREMENT QUELQUE PART !
+// todo  IL EST REFERENCE AVEC UN 'l' MINUSCULE A liip  
 // todo IL FAUT LE GARDER PARTOUT AVEC CE NOM ICI ET DANS service.yaml EN liipListener.php SANS L MAJUSCULE A liip ! 
-class liipListener 
 
+//* Ici j'utilise l'injection de service FilterService de liip imagine bundle - déclaré ensuite dans services.yaml
+//* Je crée ma méthode sur l'entité Picture et je l'attache aux evenements doctrine sur cette classe.
+//* cette méthode et sa logique est donc appellée à chaque événement doctrine sur cette classe.
+//* A chaque upload de fichier, le service liip passé dans la méthode crée le cache de l'image.
+//* Je pourrais aussi utiliser ça pour créer le cache à l'upload directement dans un contrôleur
+//* Ce me permet de ne pas générer le cache côté front lors de la requête pour servir la page.
+//* C'est un otimisation pour l'utilisateur qui aura un fichier immédiattement disponnible.
+
+class liipListener 
 {
     private $filterService;
 
@@ -26,8 +34,7 @@ class liipListener
     public function cachePictureFile(Picture $picture)
     {   
         $pictureName = $picture->getPictureFile(); // je récupère le nom du fichier
-        $path = '/assets/upload/pictures/'; //je défini le path local
-        $this->filterService->getUrlOfFilteredImage($path.$pictureName, 'portrait');
-
+        $path = '/assets/upload/pictures/'; //* je défini le path local pour lui dire où il va trouver l'image  à traiter
+        $this->filterService->getUrlOfFilteredImage($path.$pictureName, 'portrait'); //je reconstruit l'url + le nom duu fichier à traiter à l'event Doctrine et je lui passe le filtre
     }
 }
