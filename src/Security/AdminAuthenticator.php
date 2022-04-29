e0daaf41b576ffa562ce085a5cd74decf0269da5
Simon Chabrier <simonchabrier@MacBook-Pro-de-Simon.local>
2022-04-29 11:53:53 +0200

    custom redirect after login success

--- src/Security/AdminAuthenticator.php
+++ src/Security/AdminAuthenticator.php
@@ -44,12 +44,13 @@ class AdminAuthenticator extends AbstractLoginFormAuthenticator
     }
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
-    {
-        if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
-            return new RedirectResponse($targetPath);
-        }
+    {   
+        
+        // if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
+        //     return new RedirectResponse($targetPath);
+        // }
 
-        // Redirect after Login succesfull
+        // Custom redirect after Login succesfull
         return new RedirectResponse($this->urlGenerator->generate('app_admin'));
         
         throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
