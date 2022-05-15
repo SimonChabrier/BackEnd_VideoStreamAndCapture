fbc6a5f005801ea97cf231822f538c4d6352442c
Simon Chabrier <simonchabrier@MacBook-Pro-de-Simon.local>
2022-05-15 11:05:33 +0200

    clean listeners ans services

--- src/EventListener/PictureListener.php
+++ src/EventListener/PictureListener.php
@@ -3,34 +3,42 @@
 namespace App\EventListener;
 
 use App\Entity\Picture;
+// Mon service => ma méthode
 use App\Service\jpegConverterService;
-
+// Service de Liip => ses methodes
+use Liip\ImagineBundle\Service\FilterService;
 
 class PictureListener 
 {
 
-    // ici a chaque mise à jour d'une Picture
-    // je vais demander de me créer le Fichier jpeg
-    // en apellant mon service jpegConverterService
-    // le paramètrage de ce listener est déclaré dans le fichier services.yaml
+    /**
+     * Paramètre déclaré => services.yaml
+     * @var string
+     */
+    private $uploadPath;
 
-    // j'apelle le contructeur pour accèder aux propriétés de jpegConverterService service
-    public function __construct(jpegConverterService $jpegConverterService)
+    public function __construct(jpegConverterService $jpegConverterService, FilterService $liipFilterService, string $uploadPath)
     {
-        $this->jpegConverterService = $jpegConverterService; 
-  
+        $this->jpegConverterService = $jpegConverterService;
+        $this->liipFilterService = $liipFilterService;
+        $this->uploadPath = $uploadPath;
     }
 
     /**
-     * Voir services.yaml
-     * qui apelle cette méthode sur les evenements Doctrine
+     * services.yaml
+     * apelle cette méthode sur les evenements Doctrine sur la class Picture
      * @param Picture $picture
      * @return void
      */
     public function updatePictureFile(Picture $picture)
     {   
+        // On traite l'image avec la méthode de jpegConverterService pour convertir le Base64 et stocker le fichier dans un rep local
         $pictureFile = $this->jpegConverterService->convertBase64ToJgeg($picture->getPicture());
+        // On set le nom de $pictureFile avec la valeur du return de convertBase64ToJgeg()
         $picture->setPictureFile($pictureFile);
+        // On traite la mise en cache
+        $this->liipFilterService->getUrlOfFilteredImage($this->uploadPath . $picture->getPictureFile(), 'portrait'); 
+        // je reconstruit l'url + le nom du fichier à traiter et je lui passe mon filtre liip
         
     }
 }
